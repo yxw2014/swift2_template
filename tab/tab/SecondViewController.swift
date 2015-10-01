81f0923f9b6c3dc377c2c13b29b778779a5bb2b5
yxw2014 <347835460@qq.com>
2015-10-02 00:21:08 +0800

    project

---
+++ tab/tab/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tab
+//
+//  Created by yexinwei on 15/10/2.
+//  Copyright © 2015年 yexinwei. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
